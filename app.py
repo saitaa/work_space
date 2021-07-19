d89656b2989805c1750238d11a92793b82489d8b
Sait <saitaacatkal@gmail.com>
2021-07-19 04:50:59 -0400

    added sec group

---
+++ app.py
@@ -0,0 +1,17 @@
+# Import Flask modules
+from flask import Flask, render_template
+
+
+# Create an object named app
+
+
+# Create a function named `index` which uses template file named `index.html`
+# send three numbers as template variable to the app.py and assign route of no path (‘/’)
+
+
+
+# calculate sum of them using inline function in app.py, then sent the result to the
+# “number.hmtl” file and assign route of path (‘/total’).
+# When the user comes directly “/total” path, “Since this is GET
+# request, Total hasn’t been calculated” string returns to them with “number.html” file
+# Add a statement to run the Flask application which can be debugged.
